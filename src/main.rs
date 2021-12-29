cfe093a87fe7450947314f8e05dd9ab5ab829793
Ilia Konnov <ilia@sldr.xyz>
2021-12-29 15:42:04 +0300

    First version

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![no_main]
 #![no_std]
+#![feature(lang_items)]
 
 use core::panic::PanicInfo;
 
@@ -7,3 +8,6 @@ use core::panic::PanicInfo;
 fn panic(_panic: &PanicInfo<'_>) -> ! {
     loop {}
 }
+
+#[lang = "eh_personality"]
+extern "C" fn eh_personality() {}
